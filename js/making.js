145c028a35039afbdc87dfbd10764393cc8279ff
JO SO YOUNG <115414866+breeghty@users.noreply.github.com>
2023-03-15 17:38:18 +0900

    Add files via upload

---
+++ js/making.js
@@ -0,0 +1,15 @@
+document.addEventListener('click', function(e) {
+    if (e.target.matches('a[href="#"]')) {
+        e.preventDefault();
+    }
+});
+
+//menubar menuopen, nav 사라짐
+let gnbOpen = document.querySelector('.gnb .gnb_open');
+document.querySelector('.gnb .gnb_open_btn').addEventListener('click',function(){
+    gnbOpen.classList.add('on');
+});
+document.querySelector('.gnb .gnb_open .gnb_close').addEventListener('click',function(){
+    gnbOpen.classList.remove('on');
+});
+
